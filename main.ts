7ed94f930ff1cd0caf241da3eadab9adc534371a
Bryan Kok <bryan.wyern1@gmail.com>
2021-01-27 00:11:46 +0800

    Add typescript to devDependencies

--- main.ts
+++ main.ts
@@ -1,14 +1,13 @@
 const main = () => {
-	  let z = "⿳⿻[:-1]𦫻口儿⿻⿸𰀪㇄口";
-	    let output = "";
-	      for (let char of z) {
-		          output += char;
-			      console.log(char.charCodeAt(0));
-			          console.log(output);
-				    }
+  let z = "⿳⿻[:-1]𦫻口儿⿻⿸𰀪㇄口";
+  let output = "";
+  for (let char of z) {
+    output += char;
+    console.log(char.charCodeAt(0));
+    console.log(output);
+  }
 };
 
 if (require.main === module) {
-	  main();
+  main();
 }
-
